b92eae50203c982b268e0b2cbac8eb55f72f0637
AlyaLo <79606150+AlyaLo@users.noreply.github.com>
2022-09-20 12:46:29 +0300

    push

--- test-project2/src/eventBridge/index.js
+++ test-project2/src/eventBridge/index.js
@@ -3,28 +3,23 @@ import AWS from "aws-sdk";
 async function invokeСreateWelcomePost({ categoryId, userName }) {
   try {
     const eventBridge = new AWS.EventBridge({ region: process.env.AWS_PROVIDER_REGION, apiVersion: "2015-10-07" });
-    console.log(process.env.EVENT_BUS_NAME)
-    console.log(process.env.EVENT_BUS_SERVICE_SOURCE)
-    await eventBridge.putEvents(
-      {
-        Entries: [
-          {
-            Detail: JSON.stringify({
-              categoryId,
-              userName,
-              text: `Welcome to ${categoryId} category!`,
-            }),
-            DetailType: "createWelcomePost",
-            EventBusName: process.env.EVENT_BUS_NAME,
-            Source: process.env.EVENT_BUS_SERVICE_SOURCE,
-          },
-        ],
-      },
-      function (err, data) {
-        if (err) console.log(err);
-        else console.log(data + " succeessss");
-      }
-    );
+    console.log(process.env.EVENT_BUS_NAME);
+    console.log;
+    const response = await eventBridge.putEvents({
+      Entries: [
+        {
+          Detail: JSON.stringify({
+            categoryId,
+            userName,
+            text: `Welcome to ${categoryId} category!`,
+          }),
+          DetailType: "createWelcomePost",
+          EventBusName: process.env.EVENT_BUS_NAME,
+          Source: process.env.EVENT_BUS_SERVICE_SOURCE,
+        },
+      ],
+    });
+    console.log(response);
   } catch (error) {
     return {
       type: error.type,
